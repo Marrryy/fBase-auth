c79bca6e5e2209b6270fffd89eb8f76102a5d725
Marrryy <ling.310500@gmail.com>
2019-08-09 12:24:29 +0700

    handleSubmit

--- components/signUpForm.js
+++ components/signUpForm.js
@@ -1,8 +1,25 @@
 import react, { Components } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel,FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-haaa-41acd.cloudfunctions.net';
 
 class SignUpForm extends Components {
+	state = { phone:'' };
+
+	//using handleSubmit(){} will going to use this.handleSubmit.bind(this)
+	handleSubmit = () =>{
+		axios.post('${ROOT_URL}/createUser',{
+			phone: this.state.phone
+		})
+		.then(()=>{
+			axios.post('${ROOT_URL}/requestOneTimePasswords',{
+				phone:this.state.phone
+			});
+		});
+	}
+
 	render(){
 		return(
 			<View>
@@ -10,7 +27,10 @@ class SignUpForm extends Components {
 					<FormLabel>Enter Your Phone Number</FormLabel>
 					<FormInput/>
 				</View>
-				<Button title="Submit"/>
+				<Button
+				 title="Submit"
+				 onPress={this.handleSubmit}
+				 />
 			</View>
 		);
 	}
